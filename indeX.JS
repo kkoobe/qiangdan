15fbe74862718c24f0509abf25d79cb0ad1610f0
kkoobe <zhengjie@zhuozhuo.io>
2023-07-18 10:01:39 +0800

    新增权限管理功能

--- indeX.JS
+++ indeX.JS
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = new express();
-
-
-const userlist = ["z-廖川","z-郭昱林","z-刘佑萍","z-曾川","Z-黄珂","z-林川"];
+const fs = require('fs')
 app.get("/get_user_power", (req, res, next) => {
 	// 获取用户权限
-	console.log('4444')
+	const admins = ["z-廖川","z-郭昱林",'z-zj']
+	const userlist = require("./users.json")
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	let { username } = req.query;
 	console.log(username)
@@ -13,7 +12,47 @@ app.get("/get_user_power", (req, res, next) => {
 	if (userlist.includes(username)) {
 		status = 1;
 	}
+	if(admins.includes(username)){
+		status = 2;
+	}
 	res.send({ status });
+});
+app.get("/set_user_power", (req, res, next) => {
+	// 新增用户权限
+	const userlist = require("./users.json")
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	let { username } = req.query;
+	if(!userlist.includes(username)){
+		userlist.push(username)
+		fs.writeFileSync('./users.json',JSON.stringify(userlist))
+	}
+	res.send({errmsg:'操作成功',errcode:0});
+});
+// 新增关键字
+app.get("/set_keys", (req, res, next) => {
+	// 新增用户权限
+	const keywords = require("./keywords.json")
+	console.log(keywords,'122')
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	let { keyword } = req.query;
+	if(!keywords.includes(keyword)){
+		keywords.push(keyword)
+		fs.writeFileSync('./keywords.json',JSON.stringify(keywords))
+	}
+	res.send({errmsg:'操作成功',errcode:0});
+});
+// 查看关键字
+app.get("/get_keys", (req, res, next) => {
+	// 新增用户权限
+	const keywords = require("./keywords.json")
+	res.send({data:keywords,errcode:0});
+});
+// 查看权限人
+app.get("/get_users", (req, res, next) => {
+	// 新增用户权限
+	const userlist = require("./users.json")
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.send({data:userlist,errcode:0});
 });
 app.listen("1000", () => {
 	console.log("1000");
